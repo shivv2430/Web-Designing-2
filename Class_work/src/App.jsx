f12dae52e0977ada98e4d7fb61210da465db2ca4
shivv2430 <2430.shivanipal@gmail.com>
2026-04-10 23:58:17 +0530

    Experiment

---
+++ Class_work/src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+
+const students = [
+  {
+    name: "Rahul Sharma",
+    course: "Computer Science",
+    marks: 85,
+  },
+  {
+    name: "Anita Verma",
+    course: "Information Technology",
+    marks: 92,
+  },
+  {
+    name: "Rohan Gupta",
+    course: "Electronics",
+    marks: 78,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Student Information</h1>
+
+      {students.map((student, index) => (
+        <div className="card" key={index}>
+          <h2>{student.name}</h2>
+          <p>Course: {student.course}</p>
+          <p>Marks: {student.marks}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
